6d8893a1408450cb42bcd96d25b0b0f882216513
zetzetn <53105202+zetzetn@users.noreply.github.com>
2020-03-04 21:07:32 +0900

    userカラムを削除

--- db/migrate/20200229130037_create_users.rb
+++ db/migrate/20200229130037_create_users.rb
@@ -1,12 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      #タスク４追加　ここから
-      #カラム作成
-      t.string :name
-      t.integer :age
-      t.string :address
-      #タスク４追加　ここまで
       t.timestamps
     end
   end
