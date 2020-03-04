d6ba484005934d046028540d2389feeab391c7c0
Minoru Kotegawa <KotegawaMinoru@kotegawaminoru-no-MacBook-Pro.local>
2020-03-04 21:47:23 +0900

    add devise login

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,13 @@
 class MoviesController < ApplicationController
+  before_action :move_to_index, except: :index
 
-def index
-  
-end
+  def index
+
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'movies#index'
+
+  resources :users, only: :show
 end
