ccd899451de1083b232c35d8f18a40a81cdbe331
Minoru Kotegawa <KotegawaMinoru@kotegawaminoru-no-MacBook-Pro.local>
2020-03-21 09:14:18 +0900

    add question form

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -10,7 +10,29 @@ before_action :authenticate_user!
     @question = Question.find(params[:id])
   end
 
+  def new
+    @question = Question.new
+  end
 
+  def create
+    @question = Question.new(question_params)
 
+    # インスタンスの保存に成功した場合
+    if @question.save
+      flash[:success] = "質問を登録しました。"
+      redirect_to @question
+
+    # インスタンスの保存に失敗した場合
+    else
+      flash[:danger] = "質問の登録に失敗しました。"
+      render :new
+    end
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :detail)
+  end
 
 end
