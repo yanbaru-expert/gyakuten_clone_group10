4b7986e543c236f7c78623b09d5e2e6d107c2b12
Minoru Kotegawa <KotegawaMinoru@kotegawaminoru-no-MacBook-Pro.local>
2020-04-12 19:29:16 +0900

    repair php taik page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,13 +3,7 @@ class MoviesController < ApplicationController
 before_action :authenticate_user!
 
   def index
-    @movies =
-    if request.fullpath.include?("php")
-      Movie.where(genre: "php").page(params[:page]).per(10)
-    elsif request.fullpath.include?("talk")
-      Movie.where(genre: "talk").page(params[:page]).per(10)
-    else
-      Movie.where(genre: "movie").page(params[:page]).per(10)
-    end
+    genre = params[:genre] != nil ? params[:genre] : "movie"
+    @movies = Movie.where(genre: genre).page(params[:page]).per(10)
   end
 end
