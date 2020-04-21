76631ed30240da1825b0f9f3dd9801d29b86085a
MinoruKotegawa <kotegawa.1029@gmail.com>
2020-04-21 22:16:53 +0900

    admin custom

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,6 +3,6 @@ class MoviesController < ApplicationController
 before_action :authenticate_user!
 
   def index
-    @movies = Movie.where(genre: (params[:genre] || "movie")).page(params[:page]).per(10)
+    @movies = Movie.where(genre: (params[:genre] || "movie")).order(id: "ASC").page(params[:page]).per(10)
   end
 end
