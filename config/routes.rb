818915de2e79cbce10b19040c4058219d133a679
zetzetn <53105202+zetzetn@users.noreply.github.com>
2020-04-25 14:30:19 +0900

    ルーティング修正 ・questionsネスト ・aws_texts重複

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,12 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   root to: 'movies#index'
 
-
-  resources :questions, only: [:index, :show, :create, :edit, :update]
-  resources :aws_texts
   resources :movies
   resources :questions
   resources :aws_texts
   resources :lines
   resources :texts
-  resources :questions do
+  resources :questions, only: [:index, :show, :create, :edit, :update] do
     resource :solutions, only: [:create]
   end
 end
