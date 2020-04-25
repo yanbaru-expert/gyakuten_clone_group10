a962efab4ed4c6979c5fee7030eab4b4b4e5ee46
zetzetn <53105202+zetzetn@users.noreply.github.com>
2020-04-25 13:44:07 +0900

    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,15 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   root to: 'movies#index'
 
-  #回答追加
+
   resources :questions, only: [:index, :show, :create, :edit, :update]
+  resources :aws_texts
+  resources :movies
+  resources :questions
+  resources :aws_texts
+  resources :lines
+  resources :texts
   resources :questions do
     resource :solutions, only: [:create]
-
-  resources :aws_texts
-    resources :movies
-    resources :questions
-    resources :aws_texts
-    resources :lines
-    resources :texts
   end
 end
